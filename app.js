ccd59e5e46295288c8fd61f8bd9dc1bc88a64c3e
Cary Lee <carylee@gmail.com>
2016-05-16 11:13:06 -0700

    Added feature to be able to share url of a petition

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
 const $ = require('jQuery');
 const baseUrl = 'http://api.umcgc.com/';
 
-$(document).ready(() => {
+
+function view(petitionNumber) {
   const $petition = $('#petition');
-  const $petitionNumber = $('#petitionNumber');
   const $extra = $('#extra');
-  const $btn = $('button');
   const $loader = $('.loader');
+  $loader.show();
+  $petition.empty();
+  $extra.empty();
+  $extra.hide();
+  window.location.hash = `#${petitionNumber}`;
+  const petitionPromise = $.ajax({
+    url: `${baseUrl}petitions/${petitionNumber}`,
+    method: 'GET',
+    dataType: 'html',
+  });
+  petitionPromise.done(data => {
+    $petition.append(`<h1>Petition ${petitionNumber}</h1>`);
+    $loader.hide();
+    $petition.append(data);
+    $extra.show();
+  });
+  const tablePromise = $.ajax({
+    url: `${baseUrl}petitions/meta/${petitionNumber}`,
+    method: 'GET',
+    dataType: 'html',
+  });
+  tablePromise.done(data => {
+    $extra.html(data);
+  });
+}
+
+$(() => {
+  const hashMatches = window.location.hash.match(/6\d{4}$/);
+  if (hashMatches && hashMatches.length > 0) {
+    view(hashMatches[0]);
+  } else {
+    window.location.hash = '';
+  }
+
+  const $petitionNumber = $('#petitionNumber');
+  const $btn = $('button');
 
   $('form').submit((e) => {
     e.preventDefault();
-    $loader.show();
-    $petition.empty();
-    $extra.empty();
-    $extra.hide();
-    const petitionNumber = $petitionNumber.val();
-    const petitionPromise = $.ajax({
-      url: `${baseUrl}petitions/${petitionNumber}`,
-      method: 'GET',
-      dataType: 'html',
-    });
-    petitionPromise.done(data => {
-      $petition.append(`<h1>Petition ${petitionNumber}</h1>`);
-      $loader.hide();
-      $petition.append(data);
-      $extra.show();
-    });
-    const tablePromise = $.ajax({
-      url: `${baseUrl}petitions/meta/${petitionNumber}`,
-      method: 'GET',
-      dataType: 'html',
-    });
-    tablePromise.done(data => {
-      $extra.html(data);
-    });
+    view($petitionNumber.val());
+    return false;
   });
 
   $petitionNumber.on('input', () => {
